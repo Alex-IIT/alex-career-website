af70d68ac214dae44908b86d50629f378973d459
Alex-IIT <effortelevationofficial@gmail.com>
2024-11-05 15:26:03 +0530

    Added index page

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app=Flask(__name__)
 def hello_world():
   return render_template('home.html')
 
+@app.route('/index')
+def index():
+  return render_template('index.html')
+
 if __name__ == "__main__":
-  app.run(host="0.0.0.0" ,debug=True)
+  app.run(debug=True)
